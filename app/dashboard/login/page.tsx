a83d965ad6e4cc6bd388d960a2bd218effac8655
learnhubedu <hublearn14@gmail.com>
2025-03-27 19:26:03 +0530

    Update page.tsx

--- app/dashboard/login/page.tsx
+++ app/dashboard/login/page.tsx
@@ -88,9 +88,7 @@ export default function AuthPage() {
               <div className="space-y-2">
                 <div className="flex items-center justify-between">
                   <Label htmlFor="password">Password</Label>
-                  <Button variant="link" className="px-0 font-normal h-auto" type="button">
-                    Forgot password?
-                  </Button>
+              
                 </div>
                 <div className="relative">
                   <Lock className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
