34766adfdbd2f2c63e4c26fa13fba2e7b331a3bf
learnhubedu <hublearn14@gmail.com>
2025-04-06 00:07:50 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
-    domains: ['wsbvkcnlppmrtbxwucwx.supabase.co']
+    domains: [
+      "images.unsplash.com",
+      "wsbvkcnlppmrtbxwucwx.supabase.co"
+    ]
   },
   output: "standalone", // ✅ Ensures correct deployment on Netlify
   trailingSlash: true, // ✅ Helps prevent 404 errors in Netlify deployments
-  reactStrictMode: true,
-};
+}
 
-module.exports = nextConfig;
+export default nextConfig
